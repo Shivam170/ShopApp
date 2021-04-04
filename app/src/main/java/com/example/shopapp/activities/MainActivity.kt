cc40902dfd870b6401ead38000078da0333764c0
Shivam170 <shivamsharma4723@gmail.com>
2021-04-04 21:08:21 +0530

    Splash Activity

--- app/src/main/java/com/example/shopapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/shopapp/activities/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.example.shopapp
+package com.example.shopapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shopapp.R
 
-class SplashActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_main)
     }
 }
